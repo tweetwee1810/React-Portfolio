eebfc0b36b00e2b97d68958910efc00f5feafa56
Mona Lisa <tweetwee1996@gmail.com>
2023-04-19 11:21:56 -0700

    About components

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+function About() {
+    return (
+        <section className="subsection" id="About-me">
+        <div className="information">
+            <h2> About Me</h2>
+            <p> Hello, My name is Thuy Bui. I am currently in coding Bootcamp to be a website developer. </p>
+        </div>
+        <div className="head-container"></div>
+            {/* <img className="head" src="assets/portfolio img.jpg" alt="Thuy Bui profile picture"/> */}
+      </section>
+    )
+}
+export default About;
