75ac1ce26c8e1f35fbb38dc15b8a20f075df4d8a
Mona Lisa <tweetwee1996@gmail.com>
2023-04-19 11:27:58 -0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from "./components/Navigation";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
 function App() {
+
+
   return (
+    <Router> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header>
+      <Nav> </Nav>
+    </Header>
+    <main>
+     <Routes>
+      <Route path="/" element= {<About/>}/>
+      <Route path="/portfolio" element= {<Portfolio/>}/>
+      <Route path="/resume" element= {<Resume/>}/>
+      <Route path="/contact" element= {<Contact/>}/>
+
+     </Routes>
+        
+
+      
+    </main>
+     <Footer/>
     </div>
+    </Router>
   );
 }
 
